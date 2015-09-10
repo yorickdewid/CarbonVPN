b9c1241409f3a9af552fc03cc9351bdfab82775c
yorickdewid <yorick17@outlook.com>
2015-09-10 17:31:34 -0400

    Convert hex2bin

--- util.c
+++ util.c
@@ -10,6 +10,15 @@ void print_hex(unsigned char *s, size_t n) {
 	printf("\n");
 }
 
+void hextobin(unsigned char *v, unsigned char *s, size_t n) {
+	int i;
+	unsigned char *p = s;
+	for (i=0; i<n; ++i) {
+		sscanf((char *)p, "%2hhx", &v[i]);
+		p += 2 * sizeof(char);
+	}
+}
+
 char *strdup(const char *str) {
 	int n = strlen(str) + 1;
 	char *dup = malloc(n);
--- util.h
+++ util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 
 void print_hex(unsigned char *s, size_t n);
+void hextobin(unsigned char *v, unsigned char *s, size_t n);
 char *strdup(const char *str);
 
 #endif // UTIL_H
